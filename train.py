a694aae109f755a815797a4e288d9d7168c30ba6
jmpleo <totrash@duck.com>
2023-07-14 18:18:58 +0300

    opt train script, upd notebooks, add train notebook, add result pic

--- train.py
+++ train.py
@@ -94,83 +94,168 @@ timestamp_feats = [
 ]
 
 
-balanced_batches = []
+train_balanced_batches = []
+test_balanced_batches = []
+
 batch_size = 50
-print("start...", end='')
 
-disbalance = [1,10,20,30,40,50,60,70,80,90]
+train_disbalances = [1,10,20,30,40,50]
+test_disbalances = [1,10,20,30,40,50]
 
-for train_disbalanse in disbalance:
-    for batch_count in range(len(sample_list) // batch_size):
-        print(f"\rbatch n. {batch_count} initialization...", end='')
-        batch = pd.concat(
-            [
-                pd.concat(
-                    [
-                        pd.read_csv(
-                            f"dataset/netflow/flow/{filename}",
-                            names=netflow_fields,
-                            encoding="cp1251"
-                        ) for filename in sample_list[s:s+5]
-                    ],
-                    ignore_index=True
-                ).merge(
-                    alert_data,
-                    how='left',
-                    on=ident_features,
-                    indicator=True
-                ) [feats + timestamp_feats + ['_merge']]
-                for s in range(batch_size * (batch_count    ),
-                            batch_size * (batch_count + 1), 5)
-            ],
-            ignore_index=True
-        )
-        batch['is_alert'] = batch['_merge'].replace(
-            {
-                'both': 1, 'left_only': 0, 'right_only': 0
-            },
-        )
-        batch = batch[
+
+for batch_count in range(len(sample_list) // batch_size):
+    print(f"train batch n. {batch_count} initialization...", end='\r')
+    batch = pd.concat(
+        [
+            pd.concat(
+                [
+                    pd.read_csv(
+                        f"dataset/netflow/flow/{filename}",
+                        names=netflow_fields,
+                        encoding="cp1251"
+                    ) for filename in sample_list[s:s+5]
+                ],
+                ignore_index=True
+            ).merge(
+                alert_data,
+                how='left',
+                on=ident_features,
+                indicator=True
+            ) [feats + timestamp_feats + ['_merge']]
+            for s in range(batch_size * (batch_count    ),
+                        batch_size * (batch_count + 1), 5)
+        ],
+        ignore_index=True
+    )
+    batch['is_alert'] = batch['_merge'].replace(
+        {
+            'both': 1, 'left_only': 0, 'right_only': 0
+        },
+    )
+    batch = batch[
+        (
             (
-                (
-                    batch.is_alert == 1
-                ) & (
-                    batch.first_alert_time_sec >= batch.first_pkt_time_sec
-                ) & (
-                    batch.first_alert_time_sec <= batch.last_pkt_time_sec
-                ) & (
-                    batch.last_alert_time_sec <= batch.last_pkt_time_sec
-                )
-            ) | (
-                batch.is_alert == 0
+                batch.is_alert == 1
+            ) & (
+                batch.first_alert_time_sec >= batch.first_pkt_time_sec
+            ) & (
+                batch.first_alert_time_sec <= batch.last_pkt_time_sec
+            ) & (
+                batch.last_alert_time_sec <= batch.last_pkt_time_sec
             )
-        ][feats + ['is_alert']]
-
-        alert_index = batch[batch.is_alert == 1].index
-
-        balanced_batch_index = np.concatenate(
-            [
-                np.random.choice(
-                    batch[batch.is_alert == 0].index,
-                    size=alert_index.shape[0] * train_disbalanse,
-                    replace=False
-                ),
-                alert_index
-            ],
-            axis=0
+        ) | (
+            batch.is_alert == 0
         )
-        balanced_batches.append(batch.loc[balanced_batch_index])
+    ][feats + ['is_alert']]
 
+    alert_index = batch[batch.is_alert == 1].index
+
+
+    alert_index = batch[batch.is_alert == 1].index
+
+    balanced_batch_index = np.concatenate(
+        [
+            np.random.choice(
+                batch[batch.is_alert == 0].index,
+                size=alert_index.shape[0] * 100,
+                replace=False
+            ),
+            alert_index
+        ],
+        axis=0
+    )
+    train_balanced_batches.append(batch.loc[balanced_batch_index])
+
+
+for batch_count in range(len(test_list) // batch_size):
+    print(f"test batch n. {batch_count} initialization...", end='\r')
+    batch = pd.concat(
+        [
+            pd.concat(
+                [
+                    pd.read_csv(
+                        f"dataset/netflow/flow/{filename}",
+                        names=netflow_fields,
+                        encoding="cp1251"
+                    ) for filename in test_list[s:s+5]
+                ],
+                ignore_index=True
+            ).merge(
+                alert_data,
+                how='left',
+                on=ident_features,
+                indicator=True
+            ) [feats + timestamp_feats + ['_merge']]
+            for s in range(batch_size * (batch_count    ),
+                        batch_size * (batch_count + 1), 5)
+        ],
+        ignore_index=True
+    )
+    batch['is_alert'] = batch['_merge'].replace(
+        {
+            'both': 1, 'left_only': 0, 'right_only': 0
+        },
+    )
+    batch = batch[
+        (
+            (
+                batch.is_alert == 1
+            ) & (
+                batch.first_alert_time_sec >= batch.first_pkt_time_sec
+            ) & (
+                batch.first_alert_time_sec <= batch.last_pkt_time_sec
+            ) & (
+                batch.last_alert_time_sec <= batch.last_pkt_time_sec
+            )
+        ) | (
+            batch.is_alert == 0
+        )
+    ][feats + ['is_alert']]
+
+    alert_index = batch[batch.is_alert == 1].index
+
+
+    alert_index = batch[batch.is_alert == 1].index
+
+    balanced_batch_index = np.concatenate(
+        [
+            np.random.choice(
+                batch[batch.is_alert == 0].index,
+                size=alert_index.shape[0] * 100,
+                replace=False
+            ),
+            alert_index
+        ],
+        axis=0
+    )
+    test_balanced_batches.append(batch.loc[balanced_batch_index])
+
+for train_disbalance in train_disbalances:   
+    
     X = pd.concat(
-        balanced_batches,
+        train_balanced_batches,
         sort=False,
         ignore_index=True
     ).drop_duplicates()
 
-    y = X['is_alert']
+    X = X.loc[ 
+        np.concatenate(
+            [
+                np.random.choice(
+                    X[X.is_alert==0].index,
+                    size=X[X.is_alert==1].shape[0] * train_disbalance,
+                    replace=False
+                ),
+                X[X.is_alert==1].index
+            ],
+            axis=0
+        )
+    ]
+    
+    y = X.is_alert
     X = X[feats]
 
-    print(f"\ndisbalance: {X[y==1].shape[0] / X[y==0].shape[0]}")
+    print(f"train disbalance: {X[y==1].shape[0] / X[y==0].shape[0]}")
 
     ne, md = 50, 10
 
@@ -184,75 +269,31 @@ for train_disbalanse in disbalance:
     )
 
     xgb_big.fit(X, y)
-    xgb_big.save_model(f"./models/xgb_nestim-{ne}_mdepth-{md}_disbalance-{train_disbalanse}.cbm")
-    pd.DataFrame(feats).to_csv(f"./models/feats/xgb_nestim-{ne}_mdepth-{md}_disbalance-{train_disbalanse}.csv")
+    xgb_big.save_model(f"./model/xgb_nestim-{ne}_mdepth-{md}_disbalance-{train_disbalance}.cbm")
+    pd.DataFrame(feats).to_csv(f"./model/feats_xgb_nestim-{ne}_mdepth-{md}_disbalance-{train_disbalance}.csv")
 
-    for test_disbalance in disbalance:
-        balanced_batches = []
-        batch_size = 50
-        for batch_count in range(len(test_list) // batch_size):
-            batch = pd.concat(
-                [
-                    pd.concat(
-                        [
-                            pd.read_csv(
-                                f"dataset/netflow/flow/{filename}",
-                                names=netflow_fields,
-                                encoding="cp1251"
-                            ) for filename in test_list[s:s+5]
-                        ],
-                        ignore_index=True
-                    ).merge(
-                        alert_data,
-                        how='left',
-                        on=ident_features,
-                        indicator=True
-                    ) [feats + timestamp_feats + ['_merge']]
-                    for s in range(batch_size * (batch_count)   ,
-                                batch_size * (batch_count + 1), 5)
-                ],
-                ignore_index=True
-            )
-            batch['is_alert'] = batch['_merge'].replace(
-                {
-                    'both': 1, 'left_only': 0, 'right_only': 0
-                },
-            )
-            batch = batch[
-                (
-                    (
-                        batch.is_alert == 1
-                    ) & (
-                        batch.first_alert_time_sec >= batch.first_pkt_time_sec
-                    ) & (
-                        batch.first_alert_time_sec <= batch.last_pkt_time_sec
-                    ) & (
-                        batch.last_alert_time_sec <= batch.last_pkt_time_sec
-                    )
-                ) | (
-                    batch.is_alert == 0
-                )
-            ][feats + ['is_alert']]
-
-            alert_index = batch[batch.is_alert == 1].index
-            balanced_batch_index = np.concatenate(
-                [
-                    np.random.choice(
-                        batch[batch.is_alert == 0].index,
-                        size=alert_index.shape[0] * test_disbalance,
-                        replace=False
-                    ),
-                    alert_index
-                ],
-                axis=0
-            )
-            balanced_batches.append(batch.loc[balanced_batch_index])
-
+    for test_disbalance in test_disbalances:
+       
         X_test = pd.concat(
-            balanced_batches,
+            test_balanced_batches,
             sort=False,
             ignore_index=True
         )
+
+        X_test = X_test.loc[ 
+            np.concatenate(
+                [
+                    np.random.choice(
+                        X_test[X_test.is_alert == 0].index,
+                        size=X_test[X_test.is_alert==1].shape[0] * test_disbalance,
+                        replace=False
+                    ),
+                    X_test[X_test.is_alert==1].index
+                ],
+                axis=0
+            )
+        ]
+        
         y_test = X_test.is_alert
         X_test = X_test[feats]
 
@@ -263,10 +304,11 @@ for train_disbalanse in disbalance:
 
 
         plt.grid(True, color='grey', linestyle='--', linewidth=1)
-        plt.title('Precision-Recall Curve')
-        plt.xlabel('Recall')
-        plt.ylabel('Precision')
-        plt.plot(recall, precision, linewidth=2, label=f"1:{test_disbalance}")
+        plt.title(f'prc train-1:{train_disbalance}')
+        plt.xlabel('recall')
+        plt.ylabel('precision')
+        plt.plot(recall, precision, linewidth=2, label=f"test-1:{test_disbalance}")
         plt.legend()
-        plt.savefig(f"pic/prc_xgb_nestim-{ne}_mdepth-{md}_train-1:{train_disbalanse}_test-1:{test_disbalance}.png")
-        print(f"test aucpr for {train_disbalanse}:{test_disbalance} = {auc(recall, precision)}")
+        print(f"test aucpr for {train_disbalance}:{test_disbalance} = {auc(recall, precision)}")
+    plt.savefig(f"pic/prc_xgb_nestim-{ne}_mdepth-{md}_train-1:{train_disbalance}.png")
+    
